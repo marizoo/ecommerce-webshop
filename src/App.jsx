bb688a4f56b9d1023937422d5a8d6f40375d7c4b
marizoo <marizoo@meow.woof>
2022-01-16 23:31:23 +0700

    till adding cart minute 58.30

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
-const App = () => {
-  return <div>Hello world!</div>;
-};
+import React, {useState, useEffect} from 'react'
+import { commerce } from './lib/commerce'
 
-export default App;
+import { Products, Navbar } from './components';
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  }
+
+  const fetchCart = async () => {
+    const cart = await commerce.cart.retrieve();
+    setCart(cart);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  },[]);
+
+  // console.log(cart)
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <Products products={products} onHandleAddToCart={handleAddToCart}/>
+    </div>
+  )
+}
+
+export default App
+
+// https://www.youtube.com/watch?v=377AQ0y6LPA&t=265s
+// https://github.com/adrianhajdin/project_e_commerce
+//  till minute 58.30, adding to cart. maybe ID problem.
---
+++ src/components/Products/product/Product.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core'
+import { AddShoppingCart } from '@material-ui/icons'
+import useStyles from './productStyles';
+
+const Product = ({product, onHandleAddToCart}) => {
+    const classes = useStyles();
+
+    // console.log(product);
+
+
+    
+    return (
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image={product.image.url} title={product.name}/>
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography variant='h5' gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant='h5' >
+                        {product.price.formatted_with_symbol}
+                    </Typography>
+                </div>
+                <Typography dangerouslySetInnerHTML={{ __html: product.description }}  variant='body2' color='textSecondary' />
+                    
+                <CardActions disableSpacing className={classes.cardActions}>
+                    <IconButton aria-label='Add to Cart' onClick={() => onHandleAddToCart(product.id, 1)}>
+                        <AddShoppingCart />
+                    </IconButton>
+                </CardActions>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Product
